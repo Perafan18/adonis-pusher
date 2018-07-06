cfcd88802e7d2f828093975144bb21c51ad22453
Pedro Perafán <pedro_inat18@hotmail.com>
2018-07-05 23:23:50 -0500

    rename examples folder, add standard and refactor

--- instructions.js
+++ instructions.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 module.exports = async (cli) => {
   try {
-    await cli.copy(path.join(__dirname, 'examples/pusher.js'), path.join(cli.helpers.configPath(), 'pusher.js'))
+    await cli.copy(path.join(__dirname, 'stubs/pusher.js'), path.join(cli.helpers.configPath(), 'pusher.js'))
     cli.command.completed('create', 'config/pusher.js')
   } catch (error) {
     console.log(error)
--- providers/Pusher.js
+++ providers/Pusher.js
@@ -1,25 +1,26 @@
 'use strict'
 
-const { ServiceProvider, Ioc } = require('@adonisjs/fold')
+const { ServiceProvider } = require('@adonisjs/fold')
 const Pusher = require('pusher')
 
 class PusherProvider extends ServiceProvider {
   register () {
     const Config = this.app.use('Adonis/Src/Config')
-    this.app.singleton('Perafan/Pusher', (app) => {
-      return new Pusher({
-        appId: Env.get('PUSHER_APP_ID'),
-        key: Env.get('PUSHER_APP_KEY'),
-        secret: Env.get('PUSHER_SECRET_KEY'),
-        encrypted: Env.get('PUSHER_ENCRYPTED'),
-        host: Env.get('PUSHER_HOST'),
-        port: Env.get('PUSHER_PORT')
-      })
+    this.app.singleton('Perafan/Pusher', () => {
+      const config = {
+        appId: Config.get('push.appId'),
+        key: Config.get('push.key'),
+        secret: Config.get('push.secret'),
+        encrypted: Config.get('push.encrypted'),
+        host: Config.get('push.host'),
+        port: Config.get('push.port')
+      }
+      return new Pusher(config)
     })
   }
 
-  * boot() {
-    Ioc.alias('Pusher', 'Perafan/Pusher')
+  * boot () {
+    this.app.alias('Perafan/Pusher', 'Pusher')
   }
 }
 
